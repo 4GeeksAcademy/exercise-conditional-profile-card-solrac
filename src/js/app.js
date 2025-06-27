b56bd15118962ffe87de0fcaea89cf0c016cb937
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2025-06-27 19:16:57 +0000

    proyecto Generador de un perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
@@ -33,14 +34,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name || "Nombre"} ${variables.lastName ||
+    "Apellido"}</h1>
+          <h2>${variables.role || "Rol"}</h2>
+          <h3>${variables.city || "Ciudad"} <br>${variables.country ||
+    "País"} </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${
+              variables.instagram
+            }/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
